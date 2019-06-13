4e555b8fe680371fd9b1c6518fd0c3372bd14588
yurayarosh <yura12323@gmail.com>
2019-06-13 18:16:27 +0300

    add desctop part

---
+++ src/js/components/setSliders.js
@@ -0,0 +1,29 @@
+import slick from 'slick-carousel';
+import setLazy from '../components/setLazy';
+
+export default function setSliders() {
+  const $sliders = $('.js-slider');
+
+  if(!$sliders.length) return;
+
+  $sliders.each((i, slider) => {
+    const name = slider.getAttribute('data-slider');
+    const $wrap = $(slider).closest('.slider__wrap');
+    const $prev = $wrap.find('.js-prev');
+    const $next = $wrap.find('.js-next');
+
+    const options = {
+      testimonials: {
+        slidesToShow: 2,
+        prevArrow: $prev,
+        nextArrow: $next
+      }
+    };
+
+    $(slider).on('init', () => {
+      setLazy();
+    });
+
+    $(slider).slick(options[name]);
+  });
+}
