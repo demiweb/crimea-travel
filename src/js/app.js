31a8149f9cce00ca056d29d01348d38f0611aad4
yurayarosh <yura12323@gmail.com>
2019-06-13 18:19:50 +0300

    upd

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,12 @@ import setTouchClassName from './components/setTouchClassName';
 import sayHello from './lib/sayHello';
 import setLazy from './components/setLazy';
 import setSliders from './components/setSliders';
+import setDatepicker from './components/setDatepicker';
 
 $(function() {
   // sayHello();
   setTouchClassName();
   setLazy();
   setSliders();
+  setDatepicker();
 });
